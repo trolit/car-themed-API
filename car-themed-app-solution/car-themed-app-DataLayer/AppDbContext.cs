76a6702b934653defb1757ecabd70469b243cb58
Paweł Idzikowski <iddziku@gmail.com>
2020-08-15 12:48:28 +0200

    Add DbContext class and configure it in Startup

---
+++ car-themed-app-solution/car-themed-app-DataLayer/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using car_themed_app_Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace car_themed_app_DataLayer
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
--- car-themed-app-solution/car-themed-app/Startup.cs
+++ car-themed-app-solution/car-themed-app/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using car_themed_app_DataLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace car_themed_app
 {
@@ -26,6 +21,11 @@ namespace car_themed_app
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("DefaultConnection"),
+                   b => b.MigrationsAssembly("car-themed-app-DataLayer")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
