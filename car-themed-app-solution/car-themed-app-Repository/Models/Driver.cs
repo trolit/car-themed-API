bbb18e28b9923590299a6b194ffccde07c7e302a
Paweł Idzikowski <iddziku@gmail.com>
2020-08-15 12:47:50 +0200

    Create simple model to check Db generation

---
+++ car-themed-app-solution/car-themed-app-Repository/Models/Driver.cs
@@ -0,0 +1,11 @@
+﻿namespace car_themed_app_Repository.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
