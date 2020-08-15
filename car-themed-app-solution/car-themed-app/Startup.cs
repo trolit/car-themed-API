fc885bf4514966d46e76ca38f8b49150fe63314b
Paweł Idzikowski <iddziku@gmail.com>
2020-08-15 13:27:39 +0200

    Configure Swagger

--- car-themed-app-solution/car-themed-app/Startup.cs
+++ car-themed-app-solution/car-themed-app/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace car_themed_app
 {
@@ -26,6 +27,16 @@ namespace car_themed_app
                options.UseSqlServer(
                    Configuration.GetConnectionString("DefaultConnection"),
                    b => b.MigrationsAssembly("car-themed-app-DataLayer")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Car themed API",
+                    Version = "v1",
+                    Description = "Training .NET Core 3.1 API"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +46,13 @@ namespace car_themed_app
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Car themed API");
+            });
 
             app.UseHttpsRedirection();
 
