41f67050e66af0f0cd8ccf701e2fde591e8cf669
Paweł Idzikowski <iddziku@gmail.com>
2020-08-22 19:00:36 +0200

    Add class that stores routes to specific controller actions

---
+++ car-themed-app-solution/car-themed-app-Contracts/ApiRoutes.cs
@@ -0,0 +1,23 @@
+﻿namespace car_themed_app_Contracts
+{
+    public static class ApiRoutes
+    {
+        private static readonly string BaseUrl = "http://localhost:44354/api/";
+
+        public static class Dealers
+        {
+            private static readonly string _dealersControllerUrl = string.Concat(BaseUrl, "dealers");
+    
+            public static string GetAll = _dealersControllerUrl;
+
+            public static string Get = string.Concat(_dealersControllerUrl, "/{dealerId}");
+
+            public static string Delete = string.Concat(_dealersControllerUrl, "/{dealerId}");
+
+            public static string Create = _dealersControllerUrl;
+
+            public static string Update = _dealersControllerUrl;
+        }
+
+    }
+}
