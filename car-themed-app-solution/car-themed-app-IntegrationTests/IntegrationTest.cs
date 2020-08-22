56af4cf17e97bcf3b33259dfecbd903dc3c67610
Paweł Idzikowski <iddziku@gmail.com>
2020-08-22 18:08:31 +0200

    Create IntegrationTests lib and IntegrationTest base class

---
+++ car-themed-app-solution/car-themed-app-IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,40 @@
+﻿using car_themed_app;
+using car_themed_app_DataLayer;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Linq;
+using System.Net.Http;
+
+namespace car_themed_app_IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var descriptor = services.FirstOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                        if (descriptor != null)
+                        {
+                            services.Remove(descriptor);
+                        }
+
+                        services.AddDbContext<AppDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+                
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
